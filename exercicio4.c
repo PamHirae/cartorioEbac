0d7a6ffb4a376284077d95377a5015f6d1518fd7
PamHirae <149461449+PamHirae@users.noreply.github.com>
2023-11-01 04:30:11 +0900

    opçao de  saida
    incluso no sistema a opçao de  saida para o usuario

--- exercicio4.c
+++ exercicio4.c
@@ -110,9 +110,10 @@ int main()
     	printf("Escolha a opção desejada do menu:\n\n");
 	    printf("\t1 - Resgistrar nomes\n");
     	printf("\t2 - Consultar nomes\n");
-    	printf("\t3 - Deletar nomes\n\n");     
+    	printf("\t3 - Deletar nomes\n\n");   
+		printf("\t4 - Sair do sistema\n\n");
     	printf("opcao:");//Fim do menu
-	
+
     	scanf("%d", &opcao); // Armazenando a escolha do usuário
  	
     	system("cls");
@@ -131,7 +132,11 @@ int main()
 			case 3:
 			deletar();
 			break;
-		
+			
+			case 4:
+		    printf("Obrigado por utilizar o sistema!\n");
+		    return 0;
+		    break;
 			
 			default:
 			printf("Essa opção não está disponivel!");
